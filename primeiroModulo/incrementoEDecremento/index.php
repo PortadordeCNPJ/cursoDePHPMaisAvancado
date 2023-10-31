08877eff56d1e6fee6cbb42f26d225e48d94e0f0
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-10-31 10:16:54 -0300

    Att:  Incremento e Decremento, atribuindo e retirando valor a uma variavel

---
+++ primeiroModulo/incrementoEDecremento/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+//incremento/decremento
+
+$number = 10;
+
+echo ++$number; // pre-incremento ele adiciona 1 a variavel difinida
+
+echo $number++; // pós-incremento ele adiciona 1 na próxima vez que essa variavel é chamada
+
+//O mesmo se aplica ao Decremento
+?>
