154ae07b3799f1a80214be1824f2415924a3c1c5
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-10-31 10:27:57 -0300

    Att: Operadores de Comparação

---
+++ primeiroModulo/operadoresDeComparacao/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// <, >, <=, >=, !== ou !===, == ou ===
+
+// !== verifica se o valor é diferente
+// !=== verifica se o valor é diferente Ou o tipo é diferente
+// == verifica se o valor é igual
+// === verifica se o valor é igual E o tipo é igual
+
+
+$result = 50 === 50;
+
+var_dump( $result );
+
+?>
