8f851186078129135ab0b3a263ea7588b9fa0eb0
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-11-01 11:16:22 -0300

    Att: basico da estrutura do switch

---
+++ primeiroModulo/switch/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = 'guilherme';
+
+switch ($name) {
+    case 'guilherme':
+        echo 'é Guilherme';
+        break;
+    case 'pedro':
+        echo 'é pedro';
+        break;
+    case 'IIII':
+        echo 'é Ivan';
+        break;
+    default:
+        echo 'Não tem ninguem';
+        break;
+}
