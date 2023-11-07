250e2c12657cdb9c6e59e8f2f00421e7b38ecfc7
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-11-07 10:45:31 -0300

    Att: Comentarios que eu adicionei

--- primeiroModulo/funcoes/index.php
+++ primeiroModulo/funcoes/index.php
@@ -1,10 +1,13 @@
 <?php 
 
+//Função para criar uma conecção com o Banco de Dados.
 function connection(){
+    //Usando PDO e fazendo a conecção de maneira mais simples, sem alteração no código
     $pdo = new PDO('mysql:host=localhost;dbname=db_bakery','root','');
     return $pdo;
 }
 
+//Função de get para poder obter as tabelas do Banco
 function getData($tables) {
     $connection = connection();
     $query = $connection->query("select * from {$tables}");
@@ -12,6 +15,7 @@ function getData($tables) {
     return $query->fetchAll();
 }
 
+//Obtendo os valores da tabela de produtos com o Get, através de um Array
 var_dump(getData('tb_produtos'));
 
 ?>
