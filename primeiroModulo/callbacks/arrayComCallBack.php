8f948fc0e46aaf86693030b7f0ab72eef0c58d52
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-11-08 09:29:34 -0300

    Att: utilizando o array com callback, mas também usando com objeto e função

--- primeiroModulo/callbacks/arrayComCallBack.php
+++ primeiroModulo/callbacks/arrayComCallBack.php
@@ -1,12 +1,16 @@
 <?php 
 
+//criando uma classe de usuario, para passar os valores dos parametros para ele através do callback
 class User {
 
-    public  function teste(){
-        return 'teste';
+    //essa função, recebe $name e $age que são enviados do call_user_func
+    public function teste($name, $age) {
+        return 'Óla meu nome é ' . $name . ' e minha idade é ' . $age;
     }
 }
 
+//instanciando o objeto
 $user = new User;
 
-?>
+//esses valores vem através de um Array que começa com o objeto, depois vem a função que ira receber esse parametros e após iso os parametros
+echo call_user_func([$user, 'teste'], 'Guilherme', 17);
