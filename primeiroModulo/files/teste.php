8149f1b8fca11de803e9b518bef755171db2a897
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-11-17 09:38:32 -0300

    Att: enviando arquivos e imagens como files dentro do form

---
+++ primeiroModulo/files/teste.php
@@ -0,0 +1,9 @@
+<?php
+
+//if para enviar imagens e arquivos, trabalhar apenas com POST e usar o enctype="multipart/form-data"
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    
+
+    var_dump($name, $email);
+}
